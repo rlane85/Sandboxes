f4607d09a6b987cc7691ce2a684168dc7e3affac
rlane85 <44262357+rlane85@users.noreply.github.com>
2020-05-01 13:19:19 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import Icon from "@mdi/react";
+function App() {
+  return (
+    <Icon path
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
